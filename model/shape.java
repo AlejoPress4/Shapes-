49b1cc1b703e49c44ebc9f0513e5539a5956333e
Alejo Preciado <alejandro.preciado.123@gmail.com>
2023-10-08 22:22:50 -0500

    Shapes

---
+++ model/shape.java
@@ -0,0 +1,30 @@
+package model;
+public abstract class shape {
+    public int x=0;
+    public int y=0;
+    public shape (int newX, int newY){//el metodo no debe tener la misma firma, cambiar el tipo de dato y orden
+        x=newX;
+        y=newY;
+        
+    }
+    public void setX(int newX){
+        if(newX>=0&&newX<=1000){
+            x = newX;
+        }
+    }
+public int getX(){
+    return x;
+}
+
+public void setY(int newY){
+    if(newY>=0&&newY<=1000){
+        y = newY;
+    }
+}
+public int getY(){
+return y;
+}
+public abstract int getArea();
+public abstract int getPerimeter();
+}
+
